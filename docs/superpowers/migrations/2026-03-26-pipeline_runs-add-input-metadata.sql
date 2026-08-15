6ac172f920ea7d2ee03f2050ea702fd092516773
longdang193 <longdanghoangphi@gmail.com>
2026-08-15 15:14:55 +0200

    Remove obsolete specs and plans

--- docs/superpowers/migrations/2026-03-26-pipeline_runs-add-input-metadata.sql
+++
@@ -1,7 +0,0 @@
--- Add run-scoped input metadata columns to pipeline_runs
--- Run once only: BigQuery ADD COLUMN does not support IF NOT EXISTS.
-ALTER TABLE `{project}.{dataset}.pipeline_runs`
-  ADD COLUMN jobs_input_source STRING,
-  ADD COLUMN jobs_input_json STRING,
-  ADD COLUMN candidate_profile_source STRING,
-  ADD COLUMN candidate_profile_json STRING;
